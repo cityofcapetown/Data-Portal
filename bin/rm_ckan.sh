77bd0fee4a7c05a742a00ea0301946af416c2539
Gordon Inggs <gordon.inggs@gmail.com>
2019-09-20 20:39:28 +0200

    Improvements to test setup.
    * Run script made customisable
    * Splitting out datastore DB from main CKAN DB
    * Setting subnet to range which shouldn't clash with City
    * Adding in datapusher config setup

--- bin/rm_ckan.sh
+++ bin/rm_ckan.sh
@@ -5,9 +5,10 @@ docker rm -f ckan-solr
 docker rm -f ckan-datapusher
 docker rm -f ckan-minio
 docker rm -f db
+docker rm -f ckan-datastore-db
 docker rm -f ckan
 
 docker network rm ckan
 
-sudo rm -rf /tmp/ckan-config
-sudo rm -rf /tmp/ckan-storage
+#sudo rm -rf /tmp/ckan-config
+#sudo rm -rf /tmp/ckan-storage
--- bin/run_ckan.sh
+++ bin/run_ckan.sh
@@ -1,27 +1,60 @@
 #!/usr/bin/env bash
 
+tmpdir=/tmp
+default_minio_access=cctAIOSFODNN7EXAMPLE
+default_minio_secret=cctlrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+default_port=8001
+
+DATA_DIR=${1:-$tmpdir}
+MINIO_ACCESS_KEY=${2:-$default_minio_access}
+MINIO_SECRET_KEY=${3:-$default_minio_secret}
+CKAN_PORT=${4:-$default_port}
+default_hostname="http://localhost:$CKAN_PORT"
+CKAN_HOSTNAME=${5:-$default_hostname}
+
+echo Installing everything to "$DATA_DIR"
+
 # Creating network
-docker network create -d bridge ckan
+docker network create -d bridge --subnet=192.168.2.0/24 ckan
+
+# Moving CKAN config across
+mkdir -p $DATA_DIR/ckan-config
+chmod a+rw -R $DATA_DIR/ckan-config
+cp config/*.ini $DATA_DIR/ckan-config/
+
+# Making various directories
+mkdir -p $DATA_DIR/solr-data
+chmod a+rw -R $DATA_DIR/solr-data
+
+mkdir -p $DATA_DIR/ckan-db-data
+chmod a+rw -R $DATA_DIR/ckan-db-data
+
+mkdir -p $DATA_DIR/ckan-datapusher-data
+chmod a+rw -R $DATA_DIR/ckan-datapusher-data
+cp config/datapusher_settings.py $DATA_DIR/ckan-datapusher-data/
+
+mkdir -p $DATA_DIR/ckan-datastore-db-data
+chmod a+rw -R $DATA_DIR/ckan-datastore-db-data
+
+mkdir -p $DATA_DIR/ckan-minio-data
+chmod a+rw -R $DATA_DIR/ckan-minio-data
+
+# Making storage location (shouldn't be used)
+mkdir -p $DATA_DIR/ckan-storage
+chmod a+rw -R $DATA_DIR/ckan-storage
 
 # Creating supporting services
-docker run --name ckan-redis --network ckan --rm -d redis:latest
+docker run --name ckan-redis --network ckan --restart always -d redis:latest
 
-docker run --name ckan-solr --network ckan --restart always -d -v /tmp/solr-data:/opt/solr/server/solr/ckan/data ckan/solr
+docker run --name ckan-solr --network ckan --restart always -d -v $DATA_DIR/solr-data:/opt/solr/server/solr/ckan/data ckan/solr
 
-docker run --name ckan-datapusher --network ckan --restart always -d -p 8800:8800 clementmouchet/datapusher
+docker run --name ckan-datapusher --network ckan --restart always -d -p 8800:8800 -v $DATA_DIR/ckan-datapusher-data/datapusher_settings.py:/usr/src/app/deployment/datapusher_settings.py clementmouchet/datapusher
 
-docker run --name db --network ckan --restart always -d -v /tmp/ckan-postgresql-data:/var/lib/postgresql/data -e DS_RO_PASS=ckan_ro ckan/postgresql
+docker run --name db --network ckan --restart always -d -v $DATA_DIR/ckan-db-data:/var/lib/postgresql/data ckan/postgresql
 
-docker run --name ckan-minio --network ckan --restart always -d -p 9000:9000 -e "MINIO_ACCESS_KEY=cctAIOSFODNN7EXAMPLE" -e "MINIO_SECRET_KEY=cctlrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" minio/minio server /tmp/ckan-minio_data
+docker run --name ckan-datastore-db --network ckan --restart always -d -v $DATA_DIR/ckan-datastore-db-data:/var/lib/postgresql/data -e DS_RO_PASS=ckan_ro ckan/postgresql
 
-# Moving config across
-mkdir -p /tmp/ckan-config
-chmod a+rw -R /tmp/ckan-config
-cp config/*.ini /tmp/ckan-config/
-
-# Making storage location
-mkdir -p /tmp/ckan-storage
-chmod a+rw -R /tmp/ckan-storage
+docker run --name ckan-minio --network ckan --restart always -d -p 9000:9000 -e "MINIO_ACCESS_KEY=$MINIO_ACCESS_KEY" -e "MINIO_SECRET_KEY=$MINIO_SECRET_KEY" minio/minio server $DATA_DIR/ckan-minio-data
 
 # Setting up CKAN
 docker run --name ckan \
@@ -29,21 +62,24 @@ docker run --name ckan \
            --restart always \
            -d \
            -e CKAN_SQLALCHEMY_URL=postgresql://ckan:ckan@db/ckan \
-           -e CKAN_DATASTORE_WRITE_URL=postgresql://ckan:ckan@db/datastore \
-           -e CKAN_DATASTORE_READ_URL=postgresql://datastore_ro:datastore@db/datastore \
+           -e CKAN_DATASTORE_WRITE_URL=postgresql://ckan:ckan@ckan-datastore-db/datastore \
+           -e CKAN_DATASTORE_READ_URL=postgresql://datastore_ro:ckan_ro@ckan-datastore-db/datastore \
 	   -e CKAN_DATAPUSHER_URL=http://ckan-datapusher:8800 \
+	   -e CKAN_DATAPUSHER_CALLBACK_URL_BASE=http://ckan:5000/ \
 	   -e CKAN_SOLR_URL=http://ckan-solr:8983/solr/ckan \
            -e CKAN_REDIS_URL=redis://ckan-redis:6379/1 \
-	   -e CKAN_SITE_URL=http://$(hostname):8001 \
-	   -e CKAN_PORT=8001 \
+	   -e CKAN_SITE_URL=$CKAN_HOSTNAME \
+	   -e CKAN_PORT=$CKAN_PORT \
            -e POSTGRES_PASSWORD=ckan \
-           -e DS_RO_PASS=datastore \
-           -v /tmp/ckan-config:/etc/ckan \
-	   -v /tmp/ckan-storage:/var/lib/ckan \
-	   -p 8001:5000 \
+           -e DS_RO_PASS=ckan_ro \
+	   -e CKAN___CKANEXT__S3FILESTORE__AWS_ACCESS_KEY_ID="$MINIO_ACCESS_KEY" \
+	   -e CKAN___CKANEXT__S3FILESTORE__AWS_SECRET_ACCESS_KEY="$MINIO_SECRET_KEY" \
+           -v $DATA_DIR/ckan-config:/etc/ckan \
+	   -v $DATA_DIR/ckan-storage:/var/lib/ckan \
+	   -p $CKAN_PORT:5000 \
 	   -it \
 	   cityofcapetown/data-portal
 
 # Setting permissions
-docker exec ckan /usr/local/bin/ckan-paster --plugin=ckan datastore set-permissions -c /etc/ckan/production.ini | docker exec -i db psql -U ckan
+docker exec ckan /usr/local/bin/ckan-paster --plugin=ckan datastore set-permissions -c /etc/ckan/production.ini | docker exec -i ckan-datastore-db psql -U ckan
 
