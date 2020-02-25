7c410ca0a54c39d887e7883311dd7b14cc5008e2
Gordon Inggs <gordon.inggs@gmail.com>
2020-02-25 21:28:40 +0200

    k8s deploy script debugging.
    Also, adding missing configs

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 # Create CKAN namespace
 kubectl create namespace "ckan"
 
@@ -19,7 +21,6 @@ MINIO_ACCESS_KEY=${3:-$default_minio_access}
 default_minio_secret=cctlrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
 MINIO_SECRET_KEY=${4:-$default_minio_secret}
 
-
 # These will be passed in as env variables to the CKAN frontend
 kubectl create secret generic ckan-secret  --namespace ckan \
 	--from-literal="CKAN_SQLALCHEMY_URL"="postgresql://ckan:$POSTGRES_PASSWORD@db/ckan" \
@@ -33,16 +34,26 @@ kubectl create secret generic ckan-secret  --namespace ckan \
 # Creating persistent pods
 kubectl apply -f config/k8s/ckan-persistent.yaml
 
+# Waiting for db and datastore pods to be up
+datastore_pod=$(kubectl get pod --namespace ckan | grep ckan-datastore-db | cut -d " " -f1)
+db_pod=$(kubectl get pod --namespace ckan | grep ckan-db | cut -d " " -f1)
+while [[ $(kubectl get pods "$datastore_pod" --namespace ckan -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for temporary pod" && sleep 1; done
+while [[ $(kubectl get pods "$db_pod" --namespace ckan -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for temporary pod" && sleep 1; done
+
+# Correcting password setting for datastore
+kubectl exec "$datastore_pod" -i -c ckan-datastore-db --namespace ckan \
+	-- psql -U ckan -c "ALTER USER datastore_ro WITH PASSWORD '$DATASTORE_RO_PASSWORD';"
+
 # Temporary setup pod
 # Mostly for setting datastore permissions
 kubectl apply -f config/k8s/ckan-permission.yaml
-datastore_pod=$(kubectl get pod --namespace ckan | grep ckan-datastore-db | cut -d " " -f1)
 
-while [[ $(kubectl get pods ckan-datastore --namespace ckan -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for temporary pod" && sleep 1; done
-while [[ $(kubectl get pods ckan-db --namespace ckan -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for temporary pod" && sleep 1; done
-while [[ $(kubectl get pods ckan-permission-setup --namespace ckan -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for temporary pod" && sleep 1; done
+# Waiting for permission pod to come up
+ckan_permission_pod=$(kubectl get pod --namespace ckan | grep ckan-permission-setup | cut -d " " -f1)
+while [[ $(kubectl get pods "$ckan_permission_pod" --namespace ckan -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for temporary pod" && sleep 1; done
 
-kubectl exec ckan-permission-setup -c ckan-permission-setup --namespace ckan \ 
+# Configuring CKAN DB permissions - fairly awkward, we generate SQL in CKAN and pass it to the datastore DB
+kubectl exec "$ckan_permission_pod" -c ckan-permission-setup --namespace ckan \
 	-- /usr/local/bin/ckan-paster --plugin=ckan datastore set-permissions -c /etc/ckan/production.ini \
 	| kubectl exec "$datastore_pod" -i -c ckan-datastore-db --namespace ckan \
 	-- psql -U ckan
@@ -59,5 +70,5 @@ kubectl exec "$frontend_pod" -c ckan-frontend -i --namespace ckan \
 # Creating Sysadmin user
 # *Warning* interactive and echos password to screen
 frontend_pod=$(kubectl get pod --namespace ckan | grep ckan-frontend | cut -d " " -f1)
-kubectl exec "$frontend_pod" -c ckan-frontend -i --namespace ckan \
+kubectl exec "$frontend_pod" -c ckan-frontend -it --namespace ckan \
 	-- /usr/local/bin/ckan-paster --plugin=ckan sysadmin add ckan_admin email=gordon.inggs@capetown.gov.za name=ckan_admin -c /etc/ckan/production.ini
